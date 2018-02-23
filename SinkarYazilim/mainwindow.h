bcfe4a3cb49f1e1b6b4a2e1ea58b05a2a1cbfb62
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-02-23 14:33:58 +0300

    First Commit

---
+++ SinkarYazilim/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGlobal>
+
+#if QT_VERSION >= 0x050000
+#include <QtWidgets/QMainWindow>
+#else
+#include <QtGui/QMainWindow>
+#endif
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
